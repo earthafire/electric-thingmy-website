56cad79bb6b06d43b6eedb5f9efc8d322028dde2
Addison Ashworth <resiget@gmail.com>
2020-07-28 18:04:51 -0700

    formatted token

--- web/index.php
+++ web/index.php
@@ -1,1 +1,6 @@
-<p> <?php echo "hello"; ?> world </p>
+<?php 
+    $variable = $_REQUEST["token"];
+?>
+
+<p> type in the command below in your minecraft client: </p>
+<p> /thingmy oauth set <?php echo $variable; ?> </p>
